382522dcf5c4167e69804b87d21a21dac6116537
lixiaodan <lixiaodantogo@163.com>
2017-09-12 11:21:26 +0800

    做了二次修改

--- app/src/main/java/com/example/believe/graduationproject2/MainActivity.java
+++ app/src/main/java/com/example/believe/graduationproject2/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        我做了一次修改
+//        啥快递骄傲
     }
 }
